02ed0273c9bbf6f295d9c97d3ac3a3eeddc3f7dd
monody <qq614908309@gmail.com>
2024-02-03 23:21:01 +0800

    增加登录页面预览

--- app/src/main/java/com/example/ticketmachinemobile/login/LoginScreen.kt
+++ app/src/main/java/com/example/ticketmachinemobile/login/LoginScreen.kt
@@ -25,6 +25,7 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.ticketmachinemobile.R
@@ -135,4 +136,9 @@ fun LoginScreen() {
     }
 }
 
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview(){
+    LoginScreen()
+}
 
